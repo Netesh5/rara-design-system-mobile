83757541f100dee0ba87edc4128c97d65b38f405
Netesh5 <netesh.paudel@gmail.com>
2024-09-10 15:51:47 +0545

    feat: added table sample

---
+++ example/lib/features/tables/table_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+@widgetbook.UseCase(
+  name: 'Customizable',
+  type: SfDataGrid,
+)
+SfDataGrid buildTable(BuildContext context) {
+  return SfDataGrid(
+    allowSorting: true,
+    allowColumnsResizing: true,
+    allowColumnsDragging: true,
+    frozenColumnsCount: 1,
+    autoExpandGroups: true,
+    source: EmployeeDataSource(employees: getEmployees()),
+    columns: [
+      GridColumn(
+          columnName: 'id',
+          label: Container(
+              padding: const EdgeInsets.all(16.0),
+              alignment: Alignment.centerRight,
+              child: const Text(
+                'ID',
+              ))),
+      GridColumn(
+          columnName: 'name',
+          label: Container(
+              padding: const EdgeInsets.all(16.0),
+              alignment: Alignment.centerLeft,
+              child: const Text('Name'))),
+      GridColumn(
+          columnName: 'designation',
+          width: 120,
+          label: Container(
+              padding: const EdgeInsets.all(16.0),
+              alignment: Alignment.centerLeft,
+              child: const Text('Designation'))),
+      GridColumn(
+          columnName: 'salary',
+          label: Container(
+              padding: const EdgeInsets.all(16.0),
+              alignment: Alignment.centerRight,
+              child: const Text('Salary'))),
+    ],
+  );
+}
+
+class EmployeeDataSource extends DataGridSource {
+  EmployeeDataSource({required List<Employee> employees}) {
+    _employees = employees
+        .map<DataGridRow>((e) => DataGridRow(cells: [
+              DataGridCell<int>(columnName: 'id', value: e.id),
+              DataGridCell<String>(columnName: 'name', value: e.name),
+              DataGridCell<String>(
+                  columnName: 'designation', value: e.designation),
+              DataGridCell<int>(columnName: 'salary', value: e.salary),
+            ]))
+        .toList();
+  }
+
+  List<DataGridRow> _employees = [];
+
+  @override
+  List<DataGridRow> get rows => _employees;
+
+  @override
+  DataGridRowAdapter? buildRow(DataGridRow row) {
+    return DataGridRowAdapter(
+        cells: row.getCells().map<Widget>((dataGridCell) {
+      return Container(
+        alignment: (dataGridCell.columnName == 'id' ||
+                dataGridCell.columnName == 'salary')
+            ? Alignment.centerRight
+            : Alignment.centerLeft,
+        padding: const EdgeInsets.all(16.0),
+        child: Text(dataGridCell.value.toString()),
+      );
+    }).toList());
+  }
+}
+
+List<Employee> getEmployees() {
+  return [
+    Employee(10001, 'James', 'Project Lead', 20000),
+    Employee(10002, 'Kathryn', 'Manager', 30000),
+    Employee(10003, 'Lara', 'Developer', 15000),
+    Employee(10004, 'Michael', 'Designer', 15000),
+    Employee(10005, 'Martin', 'Developer', 15000),
+    Employee(10006, 'Newberry', 'Developer', 15000),
+    Employee(10007, 'Balnc', 'Developer', 15000),
+    Employee(10008, 'Perry', 'Developer', 15000),
+    Employee(10009, 'Gable', 'Developer', 15000),
+    Employee(10010, 'Grimes', 'Developer', 15000),
+    Employee(10001, 'James', 'Project Lead', 20000),
+    Employee(10002, 'Kathryn', 'Manager', 30000),
+    Employee(10003, 'Lara', 'Developer', 15000),
+    Employee(10004, 'Michael', 'Designer', 15000),
+    Employee(10005, 'Martin', 'Developer', 15000),
+    Employee(10006, 'Newberry', 'Developer', 15000),
+    Employee(10007, 'Balnc', 'Developer', 15000),
+    Employee(10008, 'Perry asdasfasfasf adsfasfasdf', 'Developer', 15000),
+    Employee(10009, 'Gable', 'Developer', 15000),
+    Employee(10010, 'Grimes', 'Developer', 15000),
+    Employee(10001, 'James', 'Project Lead', 20000),
+    Employee(10002, 'Kathryn', 'Manager', 30000),
+    Employee(10003, 'Lara', 'Developer', 15000),
+    Employee(10004, 'Michael', 'Designer', 15000),
+    Employee(10005, 'Martin', 'Developer', 15000),
+    Employee(10006, 'Newberry', 'Developer', 15000),
+    Employee(10007, 'Balnc', 'Developer', 15000),
+    Employee(10008, 'Perry', 'Developer', 15000),
+    Employee(10009, 'Gable', 'Developer', 15000),
+    Employee(10010, 'Grimes', 'Developer', 15000)
+  ];
+}
+
+class Employee {
+  Employee(this.id, this.name, this.designation, this.salary);
+  final int id;
+  final String name;
+  final String designation;
+  final int salary;
+}
--- example/lib/main.directories.g.dart
+++ example/lib/main.directories.g.dart
@@ -9,9 +9,10 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:example/features/appbar/appbar_page.dart' as _i4;
+import 'package:example/features/appbar/appbar_page.dart' as _i5;
 import 'package:example/features/bottomsheet/bottomsheet.dart' as _i2;
 import 'package:example/features/buttons/pages/button_page.dart' as _i3;
+import 'package:example/features/tables/table_page.dart' as _i4;
 import 'package:widgetbook/widgetbook.dart' as _i1;
 
 final directories = <_i1.WidgetbookNode>[
@@ -47,13 +48,25 @@ final directories = <_i1.WidgetbookNode>[
     ],
   ),
   _i1.WidgetbookFolder(
+    name: 'datagrid_widget',
+    children: [
+      _i1.WidgetbookLeafComponent(
+        name: 'SfDataGrid',
+        useCase: _i1.WidgetbookUseCase(
+          name: 'Customizable',
+          builder: _i4.buildTable,
+        ),
+      )
+    ],
+  ),
+  _i1.WidgetbookFolder(
     name: 'material',
     children: [
       _i1.WidgetbookLeafComponent(
         name: 'Scaffold',
         useCase: _i1.WidgetbookUseCase(
           name: 'Customizable',
-          builder: _i4.appBar,
+          builder: _i5.appBar,
         ),
       )
     ],
