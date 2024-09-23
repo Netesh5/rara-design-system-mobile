b75941765448b75e398b39f7e4cb18173f8ca17e
Netesh5 <netesh.paudel@gmail.com>
2024-09-24 00:16:56 +0545

    refactor: Update RaraDI.init() method signature
    This commit updates the signature of the RaraDI.init() method in the injection.dart file. The method now accepts two additional parameters: lightTheme and darkTheme, both of type ThemeData. This change allows for the customization of light and dark themes in the application.
    
    Ref: #123

--- lib/core/injection/injection.dart
+++ lib/core/injection/injection.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:rara_design_system/core/injection/pref_injector.dart';
 import 'package:rara_design_system/core/injection/theme_injector.dart';
@@ -11,9 +12,16 @@ class RaraDI {
   static init(
       {LightColors? lightColors,
       DarkColors? darkColors,
-      ITheme? appTheme}) async {
+      ITheme? appTheme,
+      ThemeData? lightTheme,
+      ThemeData? darkTheme}) async {
     await PrefInjector.init();
     await ThemeInjector.init(
-        lightColors: lightColors, darkColors: darkColors, appTheme: appTheme);
+      lightColors: lightColors,
+      darkColors: darkColors,
+      appTheme: appTheme,
+      lightTheme: lightTheme,
+      darkTheme: darkTheme,
+    );
   }
 }
